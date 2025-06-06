d0d1c1465e0bae7b00a2fb40eb6f5c1c01a93197
Hans <hans092936@gmail.com>
2025-06-06 21:45:31 +0800

    加入logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.routes import todos
+import logging
 
 app = FastAPI()
+
+logging.basicConfig(
+    filename='app.log',
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
 
 app.include_router(todos.router, prefix="/api")
